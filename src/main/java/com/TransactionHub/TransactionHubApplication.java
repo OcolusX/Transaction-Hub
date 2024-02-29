b08442c52ff804328d8d3f4742a8bce1cd3509c3
Дмитрий <melchakow.d@mail.ru>
2024-02-29 11:44:32 +0300

    Repository created

---
+++ src/main/java/com/TransactionHub/TransactionHubApplication.java
@@ -0,0 +1,13 @@
+package com.TransactionHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TransactionHub/TransactionHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TransactionHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
